663d709008cc876d44ade41398a035a2b820a7a6
Harkaitz Agirre Ezama <harkaitzv@gmail.com>
2024-09-24 00:16:09 +0200

    Remove localization.

--- CommonErrors.go
+++ CommonErrors.go
@@ -1,15 +1,19 @@
 package i18ne
 
+import (
+	"errors"
+)
+
 // UnimplementedError is a generic error that should be thrown by
 // functions that haven't been implemented.
 func UnimplementedError() (err error) {
-	err = newError(l("Unimplemented"))
+	err = errors.New("Unimplemented")
 	return
 }
 
 // InvalidArgumentError is a generic error that should be thrown by
 // functions that receive invalid arguments.
 func InvalidArgumentError() (err error) {
-	err = newError(l("Invalid argument"))
+	err = errors.New("Invalid argument")
 	return
 }
